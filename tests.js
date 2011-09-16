4cbf0aea2b3af2b6138af080162004768b27cc1a
Gerard Paapu <sharkbrainguy@gmail.com>
2011-09-16 13:11:16 +1200

    added tests

---
+++ tests.js
@@ -0,0 +1,20 @@
+var Calc = require('./calc.js');
+/*globals console: false */
+[
+    '($area x 0.05) + 10 in kg',
+    '($area % 0.96) x 1.05 in 0.96kg',
+    '$area x 0.05',
+    '(2 x $centreline_length) x (2 x $width)',
+    '(($width x $height x 0.05) x 0.02 + 5.0) in 10m3, 20m3',
+    '5 + 7',
+    '5 x 7 x 10',
+    '5 + 7 x 10',
+    '(5 + 7) x 10'
+].forEach(function (src) {
+    try {
+        console.log( src );
+        console.log( Calc.compile(src) );
+    } catch (err) {
+        console.log('failed to parse', src, err.message);
+    }
+});
